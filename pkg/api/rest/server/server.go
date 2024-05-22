2586129569bc3b845a5a1236106b5494d0e14824
ish-hcc <ish@innogrid.com>
2024-05-23 03:31:32 +0900

    api: rest: server: Print module information

--- pkg/api/rest/server/server.go
+++ pkg/api/rest/server/server.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"fmt"
+	"github.com/cloud-barista/cm-grasshopper/common"
 	"github.com/cloud-barista/cm-grasshopper/pkg/api/rest/middlewares"
+	"net"
+	"strings"
 
 	"github.com/cloud-barista/cm-grasshopper/lib/config"
 	_ "github.com/cloud-barista/cm-grasshopper/pkg/api/rest/docs" // Grasshopper Documentation
@@ -10,14 +14,57 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	infoColor   = "\033[1;34m%s\033[0m"
+	noticeColor = "\033[1;36m%s\033[0m"
+)
+
+const (
+	website = " https://github.com/cloud-barista/cm-grasshopper"
+)
+
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		logger.Println(logger.ERROR, true, err)
+		return ""
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	localIP := strings.Split(localAddr.String(), ":")
+	if len(localIP) == 0 {
+		logger.Println(logger.ERROR, true, "Failed to get local IP.")
+		return ""
+	}
+
+	return localIP[0]
+}
+
 func Init() {
 	e := echo.New()
 
 	e.Use(middlewares.CustomLogger())
 
+	// Hide Echo Banner
+	e.HideBanner = true
+
 	route.Software(e)
 	route.RegisterSwagger(e)
 	route.RegisterUtility(e)
+
+	// Display API Docs Dashboard when server starts
+	endpoint := getLocalIP() + ":" + config.CMGrasshopperConfig.CMGrasshopper.Listen.Port
+	apiDocsDashboard := " http://" + endpoint + "/" + strings.ToLower(common.ShortModuleName) + "/swagger/index.html"
+
+	fmt.Println("\n ")
+	fmt.Println(" CM-Grasshopper repository:")
+	fmt.Printf(infoColor, website)
+	fmt.Println("\n ")
+	fmt.Println(" API Docs Dashboard:")
+	fmt.Printf(noticeColor, apiDocsDashboard)
+	fmt.Println("\n ")
 
 	err := e.Start(":" + config.CMGrasshopperConfig.CMGrasshopper.Listen.Port)
 	logger.Panicln(logger.ERROR, true, err)
