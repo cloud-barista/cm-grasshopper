21cbd2d4c83f60be7a1e332a0cdb5c790d6047d3
ish-hcc <ish@innogrid.com>
2024-07-20 06:54:07 +0900

    api: rest: model: Fix tumblebug model issue

--- pkg/api/rest/model/tumblebug.go
+++ pkg/api/rest/model/tumblebug.go
@@ -1,10 +1,15 @@
 package model
 
+type CSPViewVMDetail struct {
+	PublicIP string `json:"PublicIP"`
+}
+
 type TBVMInfo struct {
-	PublicIP      string `json:"publicIP"`
-	SSHPort       string `json:"sshPort"`
-	SSHKeyID      string `json:"sshKeyId"`
-	VMUserAccount string `json:"vmUserAccount,omitempty"`
+	PublicIP        string          `json:"publicIP"`
+	SSHPort         string          `json:"sshPort"`
+	SSHKeyID        string          `json:"sshKeyId"`
+	VMUserAccount   string          `json:"vmUserAccount,omitempty"`
+	CSPViewVMDetail CSPViewVMDetail `json:"cspViewVmDetail"`
 }
 
 type TBSSHKeyInfo struct {
